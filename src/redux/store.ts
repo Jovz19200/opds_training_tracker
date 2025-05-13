736721c32d90aa5cb313ea27ff3fc7606b629de3
Jovz19200 <gisubizo.jovan@gmail.com>
2025-05-13 19:33:13 +0200

    started trainings integration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./api/loginApiSlice";
 import organizationReducer from "./api/organizationApiSlice";
+import trainingReducer from "./api/trainingApiSlice";
 
 export const store = configureStore({
   reducer: {
     login: loginReducer,
     organizations: organizationReducer,
+    trainings: trainingReducer,
   },
 });
 
