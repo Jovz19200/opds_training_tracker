0e63fdc1ffac68de2f11f6f84b1755094252cab1
Jovz19200 <gisubizo.jovan@gmail.com>
2025-06-16 04:10:19 +0200

    small fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -15,6 +15,8 @@ export const store = configureStore({
     verify2FA: verify2FAReducer,
     auth: authReducer,
   },
- 
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
